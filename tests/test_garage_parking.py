eda970680848cb1feab19159e17e33ca7a05a293
AdamHamblin <ahamblin84@gmail.com>
2019-11-18 13:08:06 -0500

    Update test_garage_parking.py

--- tests/test_garage_parking.py
+++ tests/test_garage_parking.py
@@ -1,6 +1,6 @@
-import unittest
-import sys
 import os
+import sys
+import unittest
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.abspath(os.path.join(dir_path, '..')))
